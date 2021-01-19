1c4e28496c0d4e8766ec3665a9a29b1a9d2d2bd5
alirezakohandani <alireza.ko73@gmail.com>
2021-01-19 12:42:49 +0330

    create Tag Model and define relation with Post Model

---
+++ database/migrations/2021_01_19_090521_create_relation_between_posts-and-tags-tables.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelationBetweenPostsAndTagsTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function(Blueprint $table) {
+
+            $table->unsignedInteger('post_id');
+
+            $table->unsignedInteger('tag_id');
+
+            $table->unique('post_id', 'tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
