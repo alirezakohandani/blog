c17bc920c07546bc5fc5f0efe4754560aa50b0f1
alirezakohandani <alireza.ko73@gmail.com>
2021-01-24 12:46:27 +0330

    update blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 
 @include('layouts.header')
-</head>
 <body class="bg-gray-200">
     <div class="bg-gray-800 text-white py-3 px-4 text-center fixed left-0 bottom-0 right-0 z-40">
         This a Blog Page by khatabwedaa. 
