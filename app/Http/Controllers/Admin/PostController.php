012e423d459fe24529c0a874eab909c074ec932b
alirezakohandani <alireza.ko73@gmail.com>
2021-01-25 19:22:00 +0330

    refactoring PostController

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -38,18 +38,9 @@ class PostController extends Controller
         $post->post_type = $request->post_type;
         $post->file = null;
 
-        if ($request->post_type == 'video') {
-            $request->file('file')->store('public/videos');
-            $post->file = str_replace('public/videos/', '', $request->file('file')->store('public/videos'));
-        }
-        if ($request->post_type == 'podcast') {
-            $request->file('file')->store('public/podcasts');
-            $post->file = str_replace('public/podcasts/', '', $request->file('file')->store('public/podcasts'));
-        }
+        $this->uploadImage($request, $post);
 
         $post->save();
-
-        $request->file('file')->store('public');
 
         $post->categories()->attach($request->category);
         
@@ -75,11 +66,18 @@ class PostController extends Controller
 
     }
 
-
-    // public function validation(Request $request)
-    // {
-
-    // }
+    private function uploadImage($request, $post)
+    {
+        if ($request->post_type == 'video') {
+            $request->file('file')->store('public/videos');
+            $post->file = str_replace('public/videos/', '', $request->file('file')->store('public/videos'));
+        }
+        if ($request->post_type == 'podcast') {
+            $request->file('file')->store('public/podcasts');
+            $post->file = str_replace('public/podcasts/', '', $request->file('file')->store('public/podcasts'));
+        }
+    }
+   
 
 
 }
