67405c97c65e64c253cbf23d712ad3cf9338f710
alirezakohandani <alireza.ko73@gmail.com>
2021-01-26 15:52:29 +0330

    create update post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -71,17 +71,6 @@ class postController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -90,7 +79,16 @@ class postController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+       $post = Post::findOrFail($id);
+
+       $post->update($request->all());
+
+       return response()->json([
+           'status' => 'success',
+           'data' => 'update Ok',
+       ]);
+
+
     }
 
     /**
