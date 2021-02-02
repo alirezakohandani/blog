eab23b4d2787929b5b060bbf110c7e4adcaf255e
alirezakohandani <alireza.ko73@gmail.com>
2021-02-02 17:47:36 +0330

    Create Comment View

--- Modules/Comment/Resources/views/layouts/master.blade.php
+++ Modules/Comment/Resources/views/layouts/master.blade.php
@@ -1,19 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Comment</title>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+<div class="flex mx-auto items-center justify-center shadow-lg mt-56 mx-8 mb-4 max-w-lg">
+    <form method="post" action="" class="w-full max-w-xl bg-white rounded-lg px-4 pt-2">
+       @csrf
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/comment.css') }}"> --}}
-
-    </head>
-    <body>
-        @yield('content')
-
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/comment.js') }}"></script> --}}
-    </body>
-</html>
+       
+       
+       <div class="inline-flex">
+ 
+        <input type="checkbox" name="parent_id" value="">
+        <input type="hidden" name="post_id" value="">
+       </div>
+       <br>
+    
+        reply to <b>
+        :</b>  
+   <input type="checkbox" name="parent_id" value=""> <br>
+       
+<input type="hidden" name="post_id" value="">
+       <div class="flex flex-wrap -mx-3 mb-6">
+         @include('components.alerts')
+          <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
+          <div class="w-full md:w-full px-3 mb-2 mt-2">
+             <textarea class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white" name="body" placeholder='Type Your Comment' required></textarea>
+          </div>
+          <div class="w-full md:w-full flex items-start md:w-full px-3">
+             <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
+                <svg fill="none" class="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24" stroke="currentColor">
+                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                </svg>
+                <p class="text-xs md:text-sm pt-px">Some HTML is okay.</p>
+             </div>
+             <div class="-mr-1">
+                <input type='submit' class="bg-white text-gray-700 font-medium py-1 px-4 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100" value='Post Comment'>
+             </div>
+          </div>
+       </form>
+    </div>
