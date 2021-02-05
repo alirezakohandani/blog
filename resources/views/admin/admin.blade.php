1fd0494f24ee37894ae0e338d2b9e5c3098af320
alirezakohandani <alireza.ko73@gmail.com>
2021-02-05 09:47:53 +0330

    Add Analytics Feature

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -73,12 +73,12 @@
                     </li>
                     <li class="mr-3 flex-1">
                         <a href="#" class="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-blue-600">
-                            <i class="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Analytics</span>
+                            <i class="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">Send Post</span>
                         </a>
                     </li>
                     <li class="mr-3 flex-1">
                         <a href="#" class="block py-1 md:py-3 pl-0 md:pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-red-500">
-                            <i class="fa fa-wallet pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Payments</span>
+                            <i class="fa fa-wallet pr-0 md:pr-3"></i><span class="pb-1 md:pb-0 text-xs md:text-base text-gray-600 md:text-gray-400 block md:inline-block">Analytics</span>
                         </a>
                     </li>
                 </ul>
