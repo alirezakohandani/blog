ee85b97eeb24ac903fd975a869c43b3198e4f517
alirezakohandani <alireza.ko73@gmail.com>
2021-02-05 09:55:41 +0330

    Update AnalyticsController

--- app/Http/Controllers/Admin/AnalyticsController.php
+++ app/Http/Controllers/Admin/AnalyticsController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AnalyticsController extends Controller
 {
     public function show()
     {
-        return true;
+        dd('yes');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,4 @@
 
 Route::get('/post', [App\Http\Controllers\Admin\PostController::class, 'show'])->name('show.form');
 Route::post('/post', [App\Http\Controllers\Admin\PostController::class, 'store'])->name('store.form');
-Route::get('get', [App\Http\Controllers\Admin\Analytics::class, 'show'])->name('Analytics');
+Route::get('/Analytics', [App\Http\Controllers\Admin\AnalyticsController::class, 'show'])->name('Analytics');
