a757bc7aaabdbd5f2fbaf3aa2368e415e145315f
alirezakohandani <alireza.ko73@gmail.com>
2021-02-11 11:20:06 +0330

    Route Refactoring

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,10 @@
 <?php
 
-Route::get('/post', [App\Http\Controllers\Admin\PostController::class, 'show'])->name('admin.show.form');
-Route::post('/post', [App\Http\Controllers\Admin\PostController::class, 'store'])->name('admin.store.form');
-Route::get('/Analytics', [App\Http\Controllers\Admin\AnalyticController::class, 'show'])->name('admin.Analytics');
-
-Route::get('send/sms', [App\Http\Controllers\Admin\NotificationController::class, 'show'])->name('admin.notification.show.form.sms');
-Route::post('send/sms', [App\Http\Controllers\Admin\NotificationController::class, 'sendSms'])->name('admin.notification.send.sms');
+Route::group(['as' => 'admin.', 'middleware' => 'auth'], function() {
+    Route::get('/post', [App\Http\Controllers\Admin\PostController::class, 'show'])->name('show.form');
+    Route::post('/post', [App\Http\Controllers\Admin\PostController::class, 'store'])->name('store.form');
+    Route::get('/Analytics', [App\Http\Controllers\Admin\AnalyticController::class, 'show'])->name('Analytics');
+    
+    Route::get('send/sms', [App\Http\Controllers\Admin\NotificationController::class, 'show'])->name('notification.show.form.sms');
+    Route::post('send/sms', [App\Http\Controllers\Admin\NotificationController::class, 'sendSms'])->name('notification.send.sms');
+});
