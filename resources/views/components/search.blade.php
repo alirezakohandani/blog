b7daa4c2e624e2522161d7acfe35985cbe58d20c
alirezakohandani <alireza.ko73@gmail.com>
2021-03-17 15:10:31 +0330

    Search process

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ $action }}" class="flex flex-col space-y-8" method="post" enctype="multipart/form-data">
+<form action="{{ $action }}" class="flex flex-col space-y-8" method="GET" enctype="multipart/form-data">
     {{$slot}}
     <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
         Search
